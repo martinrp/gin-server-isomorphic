40af660669d2dad52f3524ecb6d215efff66298f
martinrp <martin.ri.p@gmail.com>
2016-05-10 16:00:43 +0200

    remove old ATM files

--- client/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ATM from 'components/atm';
-
-ReactDOM.render(<ATM />, document.getElementById('atm'));
--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,8 @@ module.exports = {
         }
       },
       {
-        test: /\.less$/,
-        loader: 'style-loader!css-loader!less-loader'
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
       }
     ]
   },
